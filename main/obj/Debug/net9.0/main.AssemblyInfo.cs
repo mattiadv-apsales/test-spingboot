8b4a200ee5d78f247ac6da06eee43a8da641353e
ifthennextagency <ifthen.next.agency@gmail.com>
2025-12-14 13:34:58 +0100

    version update

--- main/obj/Debug/net9.0/main.AssemblyInfo.cs
+++ main/obj/Debug/net9.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e033ff9eef865ed6d5294a36809755f4c325be33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbbd468090ad4b215f89cae37a2a5bafd6f1369")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
